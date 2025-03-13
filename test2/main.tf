daa75272322bb08baa299618d75512a6b4ed7f1d
dfantaw <56005175+dfantaw@users.noreply.github.com>
2025-03-12 21:36:22 -0400

    Update main.tf

--- test2/main.tf
+++ test2/main.tf
@@ -2,14 +2,12 @@ provider "aws" {
   region = "us-east-1"
 }
 
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
+# Define a resource (even if it's just a placeholder)
+resource "aws_s3_bucket" "example" {
+  bucket = "example-bucket-12345-98"
+  acl    = "private"
 }
+
 # Output variable
 output "region" {
   value = "us-east-1"  # The value can be any expression, or reference a resource
