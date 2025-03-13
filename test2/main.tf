e95e8ef21106ac8077a8badff7465a81877b18d0
dfantaw <56005175+dfantaw@users.noreply.github.com>
2025-03-12 22:04:06 -0400

    Update main.tf

--- test2/main.tf
+++ test2/main.tf
@@ -2,6 +2,11 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# Output variable
+output "region2" {
+  value = "us-east-1"  # The value can be any expression, or reference a resource
+}
+
 # Define a resource (even if it's just a placeholder)
 resource "aws_s3_bucket" "example" {
   bucket = "example-bucket-12345-98"
