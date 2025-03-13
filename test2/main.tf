607885ec46a01055389e75a6ed93c09660b0cee0
dfantaw <56005175+dfantaw@users.noreply.github.com>
2025-03-12 21:11:23 -0400

    Update main.tf

--- test2/main.tf
+++ test2/main.tf
@@ -1,3 +1,9 @@
-output "s3_bucket_name" {
-  value = "This is the name of the created s3 bucket name - edited in local env"
+# Define the provider (e.g., AWS)
+provider "aws" {
+  region = "us-east-1" # Change this to your desired region
+}
+
+# Output variable
+output "region" {
+  value = "us-east-1"  # The value can be any expression, or reference a resource
 }
